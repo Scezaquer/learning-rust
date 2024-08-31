a0ecf9a649ef5f1e9ab3ba809ce62432f4a16c14
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2024-08-31 17:39:48 -0400

    removed duplicate files

--- src/mutate.rs
+++
@@ -1,9 +0,0 @@
-use rand::{rngs::StdRng, Rng};
-
-pub fn mutate(agent: &mut String, rng: &mut StdRng) {
-    let len = agent.len();
-    let index = rng.gen_range(0..len);
-    let char = rng.gen_range(32..127) as u8 as char;
-
-    agent.replace_range(index..index+1, &char.to_string());
-}
